0d7db12d0ce5cc1336b198b15b6141d255b04286
IgorRusskikh <russkikh200@mail.ru>
2023-12-12 18:28:58 +0700

    crated header and news block

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import Header from "./components/Header/Header";
+import NewsBlock from "./components/NewsBlock/NewsBlock";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <NewsBlock />
+    </>
+  );
+}
+
+export default App;
